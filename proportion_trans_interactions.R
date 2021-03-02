c7eba3039cecd1026c36c7aa79d61a4bc3bd9d09
Angela Sedeño Cacciatore <angelasedeno@gmail.com>
2021-03-02 17:14:40 +0100

    Update proportion_trans_interactions.R

--- proportion_trans_interactions.R
+++ proportion_trans_interactions.R
@@ -191,3 +191,93 @@ plot.transScore <- function(transScore, control = 'WT', plot.type = 'pileup', co
     message('Please select plot.type either pileup or perchrom')
   }
 }
+
+plot.transScore.mix <- function(transScore, control = 'WT', combine.arms = T, interArm = F){
+  require(tidyr)
+  require(dplyr)
+  require(ggplot2)
+  require(ggpubr)
+  
+  if(interArm){ mode <- 'interarm' }else{ mode <- 'trans'}
+  
+  chroms <- unique(transScore$chrom)
+  for (chr in chroms){
+    # Find centromeres
+    tmp <- transScore %>% filter(chrom == chr) %>% select(control) %>% is.na() %>% as.vector() %>% rle()
+    Ci <- match(max(tmp$lengths[tmp$values == T]), tmp$lengths)
+    bins <- transScore[transScore$chrom == chr, 'bin']
+    start <- bins[max(cumsum(tmp$lengths)[Ci-1], 0) + 1] # for those cases where Ci == 1, it will set the start as the first bin
+    end <- bins[cumsum(tmp$lengths)[Ci]]
+    
+    # Get distances to centromeres
+    transScore[with(transScore, chrom == chr & bin >= start & bin <= end), 'cenBin'] <- 0
+    if(combine.arms){
+      transScore[with(transScore, chrom == chr & bin < start), 'cenBin'] <- start - bins[bins < start]
+    }else{
+      transScore[with(transScore, chrom == chr & bin < start), 'cenBin'] <- bins[bins < start] - start
+    }
+    transScore[with(transScore, chrom == chr & bin > end), 'cenBin'] <- bins[bins > end] - end
+  }
+  # Per chrom plot
+  transScore2 <- data.frame()
+  plots_perchrom <- list()
+  samples <- which(!colnames(transScore) %in% c('chrom', 'bin', 'cenBin', control))
+  for(sample in samples){
+    for(chr in chroms){
+      dat <- transScore[transScore$chrom == chr, ]
+      tmp <- aggregate(dat[, sample] ~ cenBin, dat, mean)
+      names(tmp)[2] <- 'log_fc'
+      tmp$chrom <- chr
+      transScore2 <- rbind(transScore2, tmp)
+    }
+    
+    plt <- ggplot(transScore2 %>% mutate(chrom = factor(chrom, levels = chroms), 
+                                          chrom = factor(chrom, levels = rev(levels(chrom)))), 
+                   aes(x = cenBin, y = chrom)) +
+      geom_tile(aes(fill = log_fc, colour = log_fc)) +
+      scale_fill_gradient2(low='blue', mid='white', high='red', na.value = 'grey', midpoint = 0) +
+      scale_colour_gradient2(low='blue', mid='white', high='red', na.value = 'grey', midpoint = 0) +
+      geom_vline(xintercept = 0, color='black') +
+      labs(x = '', y = 'Chromosome', fill = paste0('log2(', colnames(transScore)[sample], '/', control, ')')) +
+      scale_x_continuous(labels = function(x) format(x/1e6)) +
+      scale_y_discrete(labels = function(x) format(sub('chr', '', x))) +
+      guides(colour=F) +
+      GENOVA:::GENOVA_THEME() +
+      theme(legend.position = c(0.15, 0.25))
+    plots_perchrom <- c(plots_perchrom, list(plt))
+  }
+  
+  plots_pileup <- list()
+  controlIdx <- match(control, colnames(transScore))
+  samples <- which(!colnames(transScore) %in% c('chrom', 'bin', 'cenBin', control))
+  for(sample in samples){
+      sample <- transScore[, sample]
+      #trend <- aggregate(sample ~ cenBin, transScore, median)
+      #trend$SD <- aggregate(sample ~ cenBin, transScore, sd)[,2]
+      
+  plot_logfc <- 
+      ggplot(data = transScore2,
+                   aes(x = cenBin)) +
+        geom_smooth(aes(x=cenBin, y=log_fc), colour=ifelse(median(transScore2$log_fc) >= 0, 'red', 'blue')) +
+        #geom_point(aes(x = cenBin, y = trans.ratio, colour = sample), size = 0.3) + #TODO:make it reusable (names)
+        #ylim(-0.5, 0.5) +
+        #scale_colour_manual(name = 'Cell line', values = c('grey', 'black')) +
+        #scale_fill_manual(name = 'Cell line', values = c('grey', 'black')) +
+        #geom_vline(xintercept = 0, color='black') +
+        #scale_y_reverse() +
+        geom_vline(xintercept = 0, color='black') +
+        scale_x_continuous(labels = function(x) format(x/1e6)) +
+        labs(x = 'Distance from centromere (Mb)', y = sprintf('Log2 fold-change of\n the proportion of %s contacts', mode)) +
+        #geom_line(data = trend, aes(x = cenBin, y = sample), color = 'red', size = 0.8) +
+        #geom_smooth(data = gather(transScore, sample, trans.ratio, samples , factor_key = T),
+        #  aes(x = cenBin, y = trans.ratio)) +
+        theme_bw() +
+        theme(legend.position = 'none')
+      
+      plots_pileup <- c(plots_pileup, list(plot_logfc))
+    }
+    
+  final <- ggarrange(plotlist = c(plots_perchrom, plots_pileup), ncol = length(samples), nrow=2, 
+            align = 'hv', heights = c(9, 2))
+  annotate_figure(final, top = text_grob(paste('Change in proportion of', mode, 'interactions')))
+}
